a65d5a5bea95cafade9c3fee635e852abe761827
Galin7777 <kazakova0309@yandex.ru>
2024-08-11 13:06:53 +0300

    Added a solution to the function

---
+++ js/calcSum/calcSum.mjs
@@ -0,0 +1,20 @@
+
+/**
+ * @function calcSum
+ * @param  {number} num1
+ * @param  {number} num2
+ * @throws{Error} Error: both arguments must be numbers
+ * @throws{Error} Error: two arguments are required
+ * @returns {number}
+ */
+
+export const calcSum = (...args) => {
+  if (args.length !== 2) {
+    throw new Error('Error: two arguments are required.');
+  }
+  const [num1, num2] = args;
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    throw new Error('Error: both arguments must be numbers.');
+  }
+  return num1 + num2;
+};
