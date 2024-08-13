8e4b20d1ee9993de8f8b03bea8835b76737021cb
Galin7777 <kazakova0309@yandex.ru>
2024-08-13 12:06:16 +0300

    Fix

--- js/calcSum/calcSum.test.mjs
+++ js/calcSum/calcSum.test.mjs
@@ -1,9 +1,8 @@
-import {calcSum} from './calcSum.mjs';
+import { calcSum } from './calcSum.mjs';
 
 try {
   console.log(calcSum(4, 3));
-  console.log(calcSum('kost', 3));
   console.log(calcSum(3));
 } catch (error) {
-    console.error(error.message);
+  console.log(error);
 }
